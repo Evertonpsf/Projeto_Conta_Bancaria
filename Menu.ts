7e6bfea6719e809a96bd735ecabfa420d9963bdf
Everton Sales <pinheiroe49@gmail.com>
2024-07-03 11:36:08 -0300

    Comecando a criar os dados da conta

--- Menu.ts
+++ Menu.ts
@@ -1,18 +1,62 @@
 import readlinesync = require("readline-sync");
 import { colors } from './src/util/colors';
+import { Conta } from "./src/model/Conta";
 
 export function main() {
 
     let opcao: number;
 
+    //objetos da classe conta
+    //novas instancia da classe conta (objetos)
+
+    const c1: Conta = new Conta(1, 1234, 1, 'julia castro', 800000.00);
+    const c2: Conta = new Conta(2, 1234, 2, 'marcela sanches', 600000.00);
+
+    // visualizando o saldo da conta 1
+    c1.visualizar();
+
+    // visualizando o saldo da conta 2
+    c2.visualizar();
+
+    //mosrtando o saldo da conta 1
+    console.log(`o saldo da conta 01 e: ${c1.saldo}`);
+
+    //atribuindo novo valor a conta 2 e atualizando o mesmo
+    c2.saldo = 900000.00;
+
+    //mostrando o saldo da conta 2 atualizado
+    console.log(`o saldo da conta 02 e: ${c2.saldo}`);
+
+    //saque na conta 01
+
+
+    console.log(`\nsacar 100 Reais da conta c1: ${c1.sacar(100)}`); //true
+    c1.visualizar();
+
+    console.log(`\nsacar 1700000.00 Reais da conta c2: ${c2.sacar(17000000)}`); //false
+    c2.visualizar();
+
+    //deposito nas contas
+
+    console.log(`\ndepositar 1000000 Reais da conta c1: `);
+    c1.depositar(1000000)
+    c1.visualizar();
+
+    console.log(`\ndepositar 17500000.33 Reais da conta c2: `);
+    c2.depositar(17500000.33)
+    c2.visualizar();
+
+
+
+
     while (true) {
 
-        console.log(colors.bg.black, colors.fg.blue, 
-                   "******************************************************");
+        console.log(colors.bg.black, colors.fg.blue,
+            "><><><><><><><><><><><><><><><><><><><><><><><><><><><");
         console.log("                                                     ");
         console.log("            INSTITUICAO PRIMEIRA SAFRA               ");
         console.log("                                                     ");
-        console.log("*****************************************************");
+        console.log("><><><><><><><><><><><><><><><><><><><><><><><><><><><");
         console.log("                                                     ");
         console.log("            1 - Criar Conta                          ");
         console.log("            2 - Listar todas as Contas               ");
@@ -24,15 +68,15 @@ export function main() {
         console.log("            8 - Transferir valores entre Contas      ");
         console.log("            9 - Sair                                 ");
         console.log("                                                     ");
-        console.log("*****************************************************");
-        console.log("                                                     ", 
-        colors.reset);
+        console.log("><><><><><><><><><><><><><><><><><><><><><><><><><><><");
+        console.log("                                                     ",
+            colors.reset);
 
         console.log("Entre com a opção desejada: ");
         opcao = readlinesync.questionInt("");
 
         if (opcao == 9) {
-            console.log(colors.fg.greenstrong, 
+            console.log(colors.fg.greenstrong,
                 "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
             sobre();
             console.log(colors.reset, "");
@@ -41,55 +85,55 @@ export function main() {
 
         switch (opcao) {
             case 1:
-                console.log(colors.fg.whitestrong, 
+                console.log(colors.fg.whitestrong,
                     "\n\nCriar Conta\n\n", colors.reset);
-                
+
                 keyPress()
                 break;
             case 2:
-                console.log(colors.fg.whitestrong, 
+                console.log(colors.fg.whitestrong,
                     "\n\nListar todas as Contas\n\n", colors.reset);
 
                 keyPress()
                 break;
             case 3:
-                console.log(colors.fg.whitestrong, 
+                console.log(colors.fg.whitestrong,
                     "\n\nConsultar dados da Conta - por número\n\n", colors.reset);
 
                 keyPress()
                 break;
             case 4:
-                console.log(colors.fg.whitestrong, 
+                console.log(colors.fg.whitestrong,
                     "\n\nAtualizar dados da Conta\n\n", colors.reset);
 
                 keyPress()
                 break;
             case 5:
-                console.log(colors.fg.whitestrong, 
+                console.log(colors.fg.whitestrong,
                     "\n\nApagar uma Conta\n\n", colors.reset);
 
                 keyPress()
                 break;
             case 6:
-                console.log(colors.fg.whitestrong, 
+                console.log(colors.fg.whitestrong,
                     "\n\nSaque\n\n", colors.reset);
 
                 keyPress()
                 break;
             case 7:
-                console.log(colors.fg.whitestrong, 
+                console.log(colors.fg.whitestrong,
                     "\n\nDepósito\n\n", colors.reset);
 
                 keyPress()
                 break;
             case 8:
-                console.log(colors.fg.whitestrong, 
+                console.log(colors.fg.whitestrong,
                     "\n\nTransferência entre Contas\n\n", colors.reset);
 
                 keyPress()
                 break;
             default:
-                console.log(colors.fg.whitestrong, 
+                console.log(colors.fg.whitestrong,
                     "\nOpção Inválida!\n", colors.reset);
 
                 keyPress()
---
+++ src/model/Conta.ts
@@ -0,0 +1,109 @@
+export class Conta {
+
+    //Definir os atributos da classe (caracteristicas) do meu objeto
+    private _numero: number;
+    private _agencia: number;
+    private _tipo: number;
+    private _titular: string;
+    private _saldo: number;
+
+    // Abaixo foi definifo o metodo construtor, responsavel por criar os objetos da classe
+    constructor(numero: number, agencia: number, tipo: number, titular: string, saldo: number) {
+        this._numero = numero;
+        this._agencia = agencia;
+        this._tipo = tipo;
+        this._titular = titular;
+        this._saldo = saldo;
+    }
+
+
+    //definidos os metodos get e set de cada atributo
+
+
+    public get numero(): number {
+        return this._numero;
+    }
+
+    public get agencia(): number {
+        return this._agencia;
+    }
+
+    public get tipo(): number {
+        return this._tipo;
+    }
+
+
+    public get titular(): string {
+        return this._titular;
+    }
+
+    public get saldo(): number {
+        return this._saldo;
+    }
+
+    public set numero(value: number) {
+        this._numero = value;
+    }
+
+    public set agencia(value: number) {
+        this._agencia = value;
+    }
+
+    public set tipo(value: number) {
+        this._tipo = value;
+    }
+
+    public set titular(value: string) {
+        this._titular = value;
+    }
+
+
+    public set saldo(value: number) {
+        this._saldo = value;
+    }
+    // metodo sacar dinheiro da conta
+
+    public sacar(valor: number): boolean {
+
+        if (this._saldo <= valor) {
+            console.log("saldo e insuficiente!");
+            return false;
+        }
+
+        this._saldo = this._saldo - valor;
+        return true;
+
+    }
+
+    //metodo depositar
+    public depositar(valor: number): void {
+        this._saldo = this._saldo + valor;
+
+    }
+
+    // metodo para visualizar todos os dados do objeto
+    public visualizar(): void {
+
+        let tipo: string = "";
+
+        switch (this._tipo) {
+            case 1:
+                tipo = "Conta corrente";
+                break;
+
+            case 2:
+                tipo = "Conta poupanca";
+                break
+        }
+
+        console.log("\n*************************************");
+        console.log("\ndados da conta");
+        console.log("\n*************************************");
+        console.log(`numero da conta: ${this._numero}`);
+        console.log(`numero da agencia: ${this._agencia}`);
+        console.log(`tipo da conta: ${tipo}`);
+        console.log(`titular da conta: ${this._titular}`);
+        console.log(`saldo conta: ${this._saldo.toFixed(2)}`);
+
+    }
+}
